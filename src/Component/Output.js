244697dca7035c2413453c15443a8f07e6cb42d9
Luteces <ebompuis.dev@gmail.com>
2019-02-12 22:33:43 +0100

    Add alt attribute to remove two warnings

--- src/Component/Output.js
+++ src/Component/Output.js
@@ -15,7 +15,7 @@ class Output extends Component {
         return(
             <div className="output">
             
-        <ul> {this.props.source.messages.map((msg) => <li><div className="user"><img className="avatar" src={msg.sender.avatar}/> {msg.sender.pseudo} </div><div className="msg"><div className="date">{msg.date}:</div>{msg.text}</div></li>)}</ul>
+        <ul> {this.props.source.messages.map((msg) => <li><div className="user"><img alt="avatar" className="avatar" src={msg.sender.avatar}/> {msg.sender.pseudo} </div><div className="msg"><div className="date">{msg.date}:</div>{msg.text}</div></li>)}</ul>
             
             </div>
             
