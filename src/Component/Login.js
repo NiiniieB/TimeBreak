89be186c9e055a677d62653086381985c0c04b6a
Luteces <ebompuis.dev@gmail.com>
2019-02-19 13:42:11 +0100

    Prettier Login.js

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,51 +1,53 @@
-import React, {Component} from "react"
-import User from "../Class/User"
+import React, { Component } from "react";
+import User from "../Class/User";
 import Socket from "./Socket";
 
-
 class Login extends Component {
-	constructor(props) {
-		super(props);
-		let avatar = "";
-		this.state = {avatar};
-	}
-	getAvatar = () => {
-		let pseudo = document.getElementById("pseudo").value;
-		// console.log("https://api.github.com/search/users?q=" + pseudo);
-		fetch("https://api.github.com/search/users?q=" + pseudo)
-			.then((avatarGitHub) => avatarGitHub.json())
-			.then((avatarGitHub) => {
-				console.log(avatarGitHub);
+  constructor(props) {
+    super(props);
+    let avatar = "";
+    this.state = { avatar };
+  }
+  getAvatar = () => {
+    let pseudo = document.getElementById("pseudo").value;
+    // console.log("https://api.github.com/search/users?q=" + pseudo);
+    fetch("https://api.github.com/search/users?q=" + pseudo)
+      .then(avatarGitHub => avatarGitHub.json())
+      .then(avatarGitHub => {
+        console.log(avatarGitHub);
 
-				if (avatarGitHub.total_count > 0) {
-					let user = new User();
-					user.create(avatarGitHub.items[0].avatar_url, avatarGitHub.items[0].login );
-					this.props.source.addUser(user);
-					this.props.source.me = user;
-					Socket.emit(JSON.stringify([{"type":1},user]));
-					this.props.callback();
-					document.getElementById("reset").reset() //Reset data from login field
-				} else {
-					alert("Error ! Wrong entry or avatar not found")
-				}
-			})
-	};
+        if (avatarGitHub.total_count > 0) {
+          let user = new User();
+          user.create(
+            avatarGitHub.items[0].avatar_url,
+            avatarGitHub.items[0].login
+          );
+          this.props.source.addUser(user);
+          this.props.source.me = user;
+          Socket.emit(JSON.stringify([{ type: 1 }, user]));
+          this.props.callback();
+          document.getElementById("reset").reset(); //Reset data from login field
+        } else {
+          alert("Error ! Wrong entry or avatar not found");
+        }
+      });
+  };
 
-	// This is here for prevent refresh from enter
-	static onSubmit(event) {
-		event.preventDefault()
-	}
-	render() {
-		return (
-			<div>
-				<form id="reset" onSubmit={Login.onSubmit}>
-					<input id="pseudo" type="text" />
-					<button onClick={this.getAvatar}  value="Login">
-						Login
-					</button>
-				</form>
-			</div>
-		)
-	}
+  // This is here for prevent refresh from enter
+  static onSubmit(event) {
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <form id="reset" onSubmit={Login.onSubmit}>
+          <input id="pseudo" type="text" />
+          <button onClick={this.getAvatar} value="Login">
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-export default Login
+export default Login;
