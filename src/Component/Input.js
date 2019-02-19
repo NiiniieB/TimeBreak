f0fdcb35cbd30dac7337737b4c297d51b751db6c
Romain Roux <cuisinier.sonore@gmail.com>
2019-02-19 16:17:55 +0100

    change type pour message envoyé

--- src/Component/Input.js
+++ src/Component/Input.js
@@ -19,10 +19,9 @@ class Input extends Component {
 
 click = () => {
   let objMesg = new Message();
- // let objUser = this.props.source.users[this.props.source.users.length - 1]; // ObjUser correspond au tableau d'objets "users" dans TimeBreak avec avatar + pseudo de la derniere personne connecte (grâce au .lenght)
   objMesg.create(this.props.source.me,"",this.getTime("true"),document.getElementById("texte").value) ;
   this.props.source.addMessage(objMesg);
-  Socket.emit(JSON.stringify([{"type":0},objMesg]));
+  Socket.emit(JSON.stringify([{"type":3},objMesg]));
   this.props.callback();
   document.getElementById("resetInput").reset(); //Reset data
   }
