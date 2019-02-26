1035f34c25c0ed2ff08fd63ddb8f197d10b1ce7c
NiiniieB <mel.betourne.14@gmail.com>
2019-02-26 11:58:53 +0100

    modif css jeu

--- src/Component/Connecter.js
+++ src/Component/Connecter.js
@@ -9,7 +9,7 @@ class Connecter extends Component {
 
             
             <div>            
-        <ul className="connecter" id="menu"> {this.props.source.users.map((user) => <li key={"index"} className="user-list" ><span className="display">{user.pseudo} </span><img alt="avatar" className="avatar" src={user.avatar}/></li>)}</ul>
+        <ul className="connecter"> {this.props.source.users.map((user) => <li key={"index"} className="user-list" ><span className="display">{user.pseudo} </span><img alt="avatar" className="avatarConnecter" src={user.avatar}/></li>)}</ul>
             
             </div>
         
